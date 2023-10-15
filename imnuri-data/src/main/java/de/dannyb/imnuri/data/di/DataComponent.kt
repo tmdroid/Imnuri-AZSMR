08bd28cb9bf8c82182a65e7aeb1e0bf0f404cc62
daniel <daniel@simvelop.de>
2023-10-15 08:54:19 +0200

    provide retrofit service via dagger

--- imnuri-data/src/main/java/de/dannyb/imnuri/data/di/DataComponent.kt
+++ imnuri-data/src/main/java/de/dannyb/imnuri/data/di/DataComponent.kt
@@ -2,7 +2,9 @@ package de.dannyb.imnuri.data.di
 
 import javax.inject.Singleton
 import com.squareup.anvil.annotations.MergeComponent
+import de.dannyb.imnuri.data.remote.ImnuriService
 
-@MergeComponent(scope = Singleton::class)
+@MergeComponent(scope = Singleton::class, modules = [NetworkModule::class])
 interface DataComponent {
+    fun getImnuriService(): ImnuriService
 }
